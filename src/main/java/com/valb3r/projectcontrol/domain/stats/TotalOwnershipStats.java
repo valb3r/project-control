53b9c67ea3cf8441a6ea79b82078bbaff9d5a24c
valb3r <valentyn.berezin@adorsys.com.ua>
2020-12-29 22:34:10 +0200

    Update ownership stats

--- src/main/java/com/valb3r/projectcontrol/domain/stats/TotalOwnershipStats.java
+++ src/main/java/com/valb3r/projectcontrol/domain/stats/TotalOwnershipStats.java
@@ -1,5 +1,6 @@
 package com.valb3r.projectcontrol.domain.stats;
 
+import com.valb3r.projectcontrol.config.annotation.OnSaveValidationGroup;
 import com.valb3r.projectcontrol.domain.Alias;
 import com.valb3r.projectcontrol.domain.GitRepo;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,10 @@ import lombok.ToString;
 import org.neo4j.ogm.annotation.GeneratedValue;
 import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -24,6 +29,15 @@ public class TotalOwnershipStats extends Accountable {
     @Id
     @GeneratedValue
     private Long id;
+
+    @NotNull(groups = OnSaveValidationGroup.class)
+    private Instant from;
+
+    @NotNull(groups = OnSaveValidationGroup.class)
+    private Instant to;
+
+    @Min(value = 0, groups = OnSaveValidationGroup.class)
+    protected long linesOwned;
 
     @Builder
     public TotalOwnershipStats(GitRepo repo, Alias alias, Long id) {
