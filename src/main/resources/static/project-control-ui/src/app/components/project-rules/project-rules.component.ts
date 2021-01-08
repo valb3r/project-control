434080bbfbd58077a6e3ea4df40e130b39180c0d
valb3r <valentyn.berezin@adorsys.com.ua>
2021-01-08 08:07:38 +0200

    More work on rules

--- src/main/resources/static/project-control-ui/src/app/components/project-rules/project-rules.component.ts
+++ src/main/resources/static/project-control-ui/src/app/components/project-rules/project-rules.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {EntityModelGitRepo} from "../../api";
+import {
+  EntityModelGitRepo,
+  FileExclusionRuleEntityControllerService,
+  FileInclusionRuleEntityControllerService
+} from "../../api";
+import {zip} from "rxjs";
 
 @Component({
   selector: 'app-project-rules',
@@ -10,9 +15,31 @@ export class ProjectRulesComponent implements OnInit {
 
   @Input() project: EntityModelGitRepo;
 
-  constructor() { }
+  isLoadingResults = true;
+  rules: Rule[];
+
+  constructor(
+    private fileInclusionRules: FileInclusionRuleEntityControllerService,
+    private fileExclusionRules: FileExclusionRuleEntityControllerService
+  ) { }
 
   ngOnInit(): void {
+    this.loadRules();
   }
 
+  private loadRules() {
+    zip(
+    ).subscribe();
+  }
+}
+
+class Rule {
+  name: string;
+  url: string;
+  type: RuleType;
+}
+
+enum RuleType {
+  FILE_INCLUDE = "FILE_INCLUDE",
+  FILE_EXCLUDE = "FILE_EXCLUDE"
 }
