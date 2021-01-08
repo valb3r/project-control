79087aa1d0e39ae87a379e77448e28d2fa0e407d
Valentyn Berezin <valb3r.ua@gmail.com>
2021-01-08 15:39:38 +0200

    More work

---
+++ src/main/java/com/valb3r/projectcontrol/domain/dto/WeeklyWorkStats.java
@@ -0,0 +1,11 @@
+package com.valb3r.projectcontrol.domain.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class WeeklyWorkStats {
+
+    private final int commits;
+}
--- src/main/java/com/valb3r/projectcontrol/repository/UserRepository.java
+++ src/main/java/com/valb3r/projectcontrol/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.valb3r.projectcontrol.repository;
 
 import com.valb3r.projectcontrol.domain.User;
+import com.valb3r.projectcontrol.domain.dto.WeeklyWorkStats;
 import com.valb3r.projectcontrol.repository.domain.UserExcerpt;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
@@ -8,7 +9,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 
 import static com.valb3r.projectcontrol.config.Neo4jFullTextIndexManagement.USER_IDX;
 
@@ -21,4 +24,9 @@ public interface UserRepository extends Neo4jRepository<User, Long> {
 
     @Query("MATCH (u:User)-[a:ALIAS]->(o:Alias)-[:OF]->(r:GitRepo) WHERE id(r) = $repoId RETURN u,a,o")
     List<User> findByRepoId(@Param("repoId") Long repoId);
+
+    @Query("MATCH (u:User)-[:ALIAS]->(a:Alias)-[:OF]->(r:GitRepo) WHERE id(r) = $repoId AND id(u) = $userId " +
+        "WITH a,r MATCH (a)<-[:OF]-(w:WeeklyCommitStats)-[:OF]->(r:GitRepo) WHERE w.from = $from " +
+        "RETURN [sum(w.commitCount)]")
+    Optional<WeeklyWorkStats> getWeeklyWorkStats(@Param("userId") Long userId, @Param("repoId") Long repoId, @Param("from") Instant start);
 }
