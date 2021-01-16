97256e10ea310c69f6c4a408085f20143eb993da
valb3r <valentyn.berezin@adorsys.com.ua>
2021-01-16 15:27:19 +0200

    Rule edit and save handling

--- src/main/resources/static/project-control-ui/src/app/components/project-rules/project-rules.component.ts
+++ src/main/resources/static/project-control-ui/src/app/components/project-rules/project-rules.component.ts
@@ -2,14 +2,15 @@ import {Component, Input, NgZone, OnInit, ViewChild} from '@angular/core';
 import {
   EntityModelFileExclusionRule,
   EntityModelFileInclusionRule,
-  EntityModelGitRepo,
-  FileExclusionRuleEntityControllerService,
-  FileInclusionRuleEntityControllerService
+  EntityModelGitRepo, FileExclusionRule,
+  FileExclusionRuleEntityControllerService, FileInclusionRule,
+  FileInclusionRuleEntityControllerService, GitRepo
 } from "../../api";
 import {zip} from "rxjs";
 import {CdkTextareaAutosize} from "@angular/cdk/text-field";
 import {take} from "rxjs/operators";
 import {Form, NgForm} from "@angular/forms";
+import {Id} from "../../id";
 
 @Component({
   selector: 'app-project-rules',
@@ -43,42 +44,83 @@ export class ProjectRulesComponent implements OnInit {
   }
 
   addInclusionRule() {
+    if (this.newInclusionRule) {
+      this.expandedInclusionRule = undefined;
+      return;
+    }
+
     this.newInclusionRule = {
       name: "",
-      rule: ""
+      rule: "",
+      repo: this.project._links.self.href as any
     } as EntityModelFileInclusionRule;
   }
 
   addExclusionRule() {
+    if (this.newExclusionRule) {
+      this.newExclusionRule = undefined;
+      return;
+    }
+
     this.newExclusionRule = {
       name: "",
-      rule: ""
+      rule: "",
+      repo: this.project._links.self.href as any
     } as EntityModelFileExclusionRule
   }
 
-  updateInclusionRule(rule: EntityModelFileInclusionRule) {
-  }
-
-  updateExclusionRule(rule: EntityModelFileExclusionRule) {
-  }
-
   removeInclusionRule(rule: EntityModelFileInclusionRule) {
+    this.fileInclusionRules.deleteItemResourceFileinclusionruleDelete(Id.read(rule._links.self.href))
+      .subscribe(_ => this.loadRules());
   }
 
   removeExclusionRule(rule: EntityModelFileExclusionRule) {
+    this.fileExclusionRules.deleteItemResourceFileexclusionruleDelete(Id.read(rule._links.self.href))
+      .subscribe(_ => this.loadRules());
   }
 
   toggleExpandInclusionRule(rule: EntityModelFileInclusionRule) {
+    if (this.expandedInclusionRule === rule) {
+      this.expandedInclusionRule = undefined;
+      return;
+    }
+
+    this.expandedInclusionRule = rule;
   }
 
   toggleExpandExclusionRule(rule: EntityModelFileExclusionRule) {
+    if (this.expandedExclusionRule === rule) {
+      this.expandedExclusionRule = undefined;
+      return;
+    }
+
+    this.expandedExclusionRule = rule;
   }
 
-  saveRule(rule: EntityModelFileExclusionRule|EntityModelFileExclusionRule, type: RuleType) {
+  saveRule(rule: EntityModelFileInclusionRule|EntityModelFileExclusionRule, type: RuleType) {
     if (this.detailsForm.invalid) {
       return
     }
-    console.log("Submit");
+
+    switch (type) {
+      case RuleType.EXCLUSION:
+        this.fileExclusionRules.patchItemResourceFileexclusionrulePatch(Id.read(rule._links.self.href), rule as FileExclusionRule)
+          .subscribe(_ => this.loadRules());
+        break
+      case RuleType.INCLUSION:
+        this.fileInclusionRules.patchItemResourceFileinclusionrulePatch(Id.read(rule._links.self.href), rule as FileExclusionRule)
+          .subscribe(_ => this.loadRules());
+        break
+      case RuleType.NEW_EXCLUSION:
+        this.fileExclusionRules.postCollectionResourceFileexclusionrulePost(rule as FileExclusionRule)
+          .subscribe(_ => this.loadRules());
+        break
+      case RuleType.NEW_INCLUSION:
+        this.fileInclusionRules.postCollectionResourceFileinclusionrulePost(rule as FileInclusionRule)
+          .subscribe(_ => this.loadRules());
+        break
+      default: throw Error("Unimplemented: " + type);
+    }
   }
 
   private loadRules() {
@@ -88,8 +130,10 @@ export class ProjectRulesComponent implements OnInit {
       this.fileExclusionRules.getCollectionResourceFileexclusionruleGet1()
     ).subscribe(res => {
       this.isLoadingResults = false;
-      this.inclusionRules = res[0]._embedded.fileInclusionRules;
-      this.exclusionRules = res[1]._embedded.fileExclusionRules;
+      this.inclusionRules = res[0]._embedded.fileInclusionRules.map(it => {it.repo = this.project._links.self.href as any; return it});
+      this.exclusionRules = res[1]._embedded.fileExclusionRules.map(it => {it.repo = this.project._links.self.href as any; return it});
+      this.expandedInclusionRule = undefined;
+      this.expandedExclusionRule = undefined;
     });
   }
 
@@ -106,5 +150,7 @@ export class ProjectRulesComponent implements OnInit {
 
 export enum RuleType {
   INCLUSION = 'inclusion',
-  EXCLUSION = 'exclusion'
+  EXCLUSION = 'exclusion',
+  NEW_INCLUSION = 'new inclusion',
+  NEW_EXCLUSION = 'new exclusion'
 }
