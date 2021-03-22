d5d974045af2d2eb014e4ff976b2d1bbf068f34d
Valentyn Berezin <vbe@adorsys.com.ua>
2021-03-22 14:55:40 +0200

    Update script

--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 . environment.sh
-java -jar project-control-*.jar --spring.profiles.active=ssl
+# Force TLSv1.2 due to https://stackoverflow.com/questions/60955206/java-11-sslhandshakeexception-with-tls-1-3-how-do-i-revert-to-tls-1-2
+java -Dserver.ssl.enabled-protocols=TLSv1.2 -jar project-control-*.jar --spring.profiles.active=ssl
