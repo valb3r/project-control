0e7175f6fe07f9b3c31b4e2f4b8d5e826e959296
valb3r <valb3r.ua@gmail.com>
2021-11-08 09:19:32 +0200

    Switch to library

--- src/main/java/com/valb3r/projectcontrol/config/letsencrypt/LetsEncryptConfig.java
+++ src/main/java/com/valb3r/projectcontrol/config/letsencrypt/LetsEncryptConfig.java
@@ -1,8 +1,10 @@
 package com.valb3r.projectcontrol.config.letsencrypt;
 
 import com.github.valb3r.letsencrypthelper.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
 @Import(TomcatWellKnownLetsEncryptChallengeEndpointConfig.class)
 public class LetsEncryptConfig {
 }
