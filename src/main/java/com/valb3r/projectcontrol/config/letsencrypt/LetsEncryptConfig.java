f778552d9caf262537a38057b4a00dfe5e0950fc
valb3r <valb3r.ua@gmail.com>
2021-11-25 11:58:20 +0200

    Update LetsEncrypt helper

--- src/main/java/com/valb3r/projectcontrol/config/letsencrypt/LetsEncryptConfig.java
+++ src/main/java/com/valb3r/projectcontrol/config/letsencrypt/LetsEncryptConfig.java
@@ -1,6 +1,6 @@
 package com.valb3r.projectcontrol.config.letsencrypt;
 
-import com.github.valb3r.letsencrypthelper.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
+import com.github.valb3r.letsencrypthelper.tomcat.TomcatWellKnownLetsEncryptChallengeEndpointConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
